d3d3d8c8276d78f5577d12ce22805dd3af974a56
CtrlZombie <ivan.kletnov95@gmail.com>
2025-09-06 22:14:06 +0500

    Задание к уроку Props

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+function UserCard({name, age}) {
+  return (
+    <div className = 'user-card'>
+      <h2>{name}</h2>
+      <h2>{age} Лет</h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <UserCard name = 'Иван' age = {30}/>
+      <UserCard name = 'Екатерина' age = {29}/>
+      <UserCard name = 'Михаил' age = {5}/>
+    </div>
+  )
+}
+
+export default App;
